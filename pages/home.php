55175e2cd52b886a688f379eb58bfeeeb0586d05
glaucoSapucaia <glaucopco@gmail.com>
2024-05-31 13:23:55 -0300

    home.php

---
+++ pages/home.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Home - Help Desk</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+
+        <!-- CSS -->
+        <style>
+            .card-home {
+                padding: 30px 0 0 0;
+                width: 100%;
+                margin: 0 auto;
+            }
+        </style>
+    </head>
+
+    <body>
+
+        <!-- nav -->
+        <nav class="navbar navbar-dark bg-dark">
+            <a href="#" class="navbar-brand p-3">
+                <img src="../img/logo.jpg" width='30' height='30' alt="" class="d-inline-block align-top me-2">
+                App Help Desk
+            </a>
+        </nav>
+
+        <!-- container -->
+        <div class="container">
+            <!-- row -->
+            <div class="row">
+                <div class="card-home">
+                    <div class="card">
+                        <!-- head -->
+                        <div class="card-header">
+                            Menu
+                        </div>
+
+                        <!-- body -->
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-6 d-flex justify-content-center">
+                                    <img src="../img/cria_chamado.jpg" width='70' height='70' alt="">
+                                </div>
+
+                                <div class="col-6 d-flex justify-content-center">
+                                    <img src="../img/consulta_chamado.jpg" width='70' height='70' alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
