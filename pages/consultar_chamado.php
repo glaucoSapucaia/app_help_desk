9530cb4473b79a62337a4c08216bbcabc88080a7
glaucoSapucaia <glaucopco@gmail.com>
2024-05-31 13:52:59 -0300

    consultar_chamado.php

---
+++ pages/consultar_chamado.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Consultar Chamado - Help Desk</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+
+        <!-- CSS -->
+        <style>
+            .card-consultar-chamado {
+                padding: 30px 0 0 0;
+                width: 100%;
+                margin: 0 auto;
+            }
+        </style>
+    </head>
+
+    <body>
+
+        <!-- nav -->
+        <nav class="navbar navbar-dark bg-dark">
+            <a href="#" class="navbar-brand p-3">
+                <img src="../img/logo.jpg" width='30' height='30' alt="" class="d-inline-block align-top me-2">
+                App Help Desk
+            </a>
+        </nav>
+
+        <!-- container -->
+        <div class="container">
+            <!-- row -->
+            <div class="row">
+                <!-- card -->
+                <div class="card-consultar-chamado">
+                    <div class="card">
+                        <!-- head -->
+                        <div class="card-header">
+                            Consulta de chamado
+                        </div>
+
+                        <!-- body -->
+                        <div class="card-body">
+
+                            <!-- card 1 -->
+                            <div class="card mb-3 bg-light">
+                                <div class="card-body">
+                                    <h5 class="card-title">Titulo chamado...</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
+                                    <p class="card-text">Descrição chamado...</p>
+                                </div>
+                            </div>
+
+                            <!-- card 2 -->
+                            <div class="card mb-3 bg-light">
+                                <div class="card-body">
+                                    <h5 class="card-title">Titulo chamado...</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
+                                    <p class="card-text">Descrição chamado...</p>
+                                </div>                             
+                            </div>
+
+                            <!-- btn -->
+                            <div class="row mt-5">
+                                <div class="d-grid">
+                                    <button class="btn btn-lg btn-warning" type='submit'>Voltar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
